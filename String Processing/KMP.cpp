a09f916fb16c8998c9fdce32e2072e09057244da
YazanZebak <yazanzk16@gmail.com>
2019-07-12 15:12:27 +0300

    Update KMP

--- String Processing/KMP.cpp
+++ String Processing/KMP.cpp
@@ -17,7 +17,7 @@ vector<int> KMP(string str, string pat) {
     int m = (int)pat.size();
 
     vector<int> ret;
-    vector<int> prefix = prefixFunc(pat);
+    vector<int> prefix = prefixFunction(pat);
 
     for (int i = 0 , k = 0 ; i < n ; i++) {
         while (k > 0 && pat[k] != str[i])
@@ -31,4 +31,21 @@ vector<int> KMP(string str, string pat) {
         }
     }
     return ret;
+}
+
+// Given string S for each prefix count its frequency
+void count(string &s){
+
+	 vector<int> pi = prefixFunction(s);
+	 int n = (int)pi.size() ;
+
+	 vector<int> freq(n + 1);
+
+	 for(int i = 0 ; i < n ; i++)
+           ++freq[pi[i]];
+
+	 for(int i = n - 1 ; i > 0 ; i--)
+	 	   freq[pi[i - 1]] += freq[i];
+
+	 freq.erase(freq.begin());
 }
